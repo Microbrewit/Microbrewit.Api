6713371b03b97c9d3a4f57f1152f0f775b0e15d8
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2016-04-14 18:49:15 +0200

    Added userservice

---
+++ Service/Component/UserService.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microbrewit.Api.ElasticSearch.Interface;
+using Microbrewit.Api.Model.Database;
+using Microbrewit.Api.Model.DTOs;
+using Microbrewit.Api.Repository.Interface;
+using Microbrewit.Api.Service.Interface;
+
+namespace Microbrewit.Api.Service.Component
+{
+    public class UserService : IUserService
+    {
+        IUserRepository _userRepository;
+        IUserElasticsearch _userElasticsearch;
+        
+        public UserService(IUserRepository userRepository, IUserElasticsearch userElasticsearch)
+        {
+            _userRepository = userRepository;
+            _userElasticsearch = userElasticsearch;
+        }
+        
+        public Task<UserDto> AddAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> DeleteAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<UserDto>> GetAllAsync(int from, int size)
+        {
+            var users = await _userRepository.GetAllAsync();
+            return AutoMapper.Mapper.Map<IEnumerable<User>,IList<UserDto>>(users);
+        }
+
+        public async Task<UserDto> GetSingleAsync(string userId)
+        {
+            var user = await _userRepository.GetSingleAsync(userId);
+            return AutoMapper.Mapper.Map<User,UserDto>(user);
+        }
+
+        public Task<IEnumerable<NotificationDto>> GetUserNotificationsAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ReIndexBeerRelationElasticSearch(BeerDto beerDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ReIndexBreweryRelationElasticSearch(BreweryDto breweryDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ReIndexElasticSearch()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ReIndexUserElasticSearch(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<UserDto>> SearchAsync(string query, int from, int size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(UserDto userDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateNotification(string username, NotificationDto notificationDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
