8f4d2844dd0e07e4226abeab15ea9edf63cc234c
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2016-09-30 21:59:10 +0200

    Fixed substitute resolver for hops

--- src/Microbrewit.Api/Mapper/CustomResolvers/SubstitutResolver.cs
+++ src/Microbrewit.Api/Mapper/CustomResolvers/SubstitutResolver.cs
@@ -7,37 +7,18 @@ namespace Microbrewit.Api.Mapper.CustomResolvers
 {
     public class SubstitutResolver : ValueResolver<HopDto, IList<Hop>>
     {
-        // private IHopElasticsearch _hopElasticsearch = new HopElasticsearch();
-        // private IHopRepository _hopRepository = new HopDapperRepository();
-
         protected override IList<Hop> ResolveCore(HopDto dto)
         {
-                List<Hop> hops = new List<Hop>();
-//             if (dto.Substituts == null) return hops;
-//             if (dto.Substituts != null)
-//             {
-//                 foreach (var sub in dto.Substituts)
-//                 {
-// 
-//                     if (sub.Id > 0)
-//                     {
-//                         var hop = _hopRepository.GetSingle(sub.Id);
-//                         if (hop != null)
-//                         {
-//                             hops.Add(hop);
-//                         }
-//                     }
-//                     else
-//                     {
-//                         var hopDtos = _hopElasticsearch.Search(sub.Name, 0, 1).FirstOrDefault();
-// 
-//                         if (hopDtos != null)
-//                         {
-//                             hops.Add(Mapper.Map<HopDto, Hop>(hopDtos));
-//                         }
-//                     }
-//                 }
-//             }
+            List<Hop> hops = new List<Hop>();
+            if (dto.Substituts == null) return hops;
+            foreach (var substitute in dto.Substituts)
+            {
+                var hop = new Hop {
+                    HopId = substitute.Id,
+                    Name = substitute.Name
+                };
+                hops.Add(hop);
+            }
             return hops;
         }
     }
